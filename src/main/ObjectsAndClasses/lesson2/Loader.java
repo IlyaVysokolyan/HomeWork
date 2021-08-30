77aa0e5671dfbaa1d0b8aeae13ce9a431176a9bc
Ilya <rd2d@list.ru>
2021-08-30 21:10:12 +0300

    complete lesson 2

---
+++ src/main/ObjectsAndClasses/lesson2/Loader.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class Loader {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.feed(150.0);
+        cat.pee();
+        
+        System.out.println(cat.getSumFeed());
+
+    }
+}
