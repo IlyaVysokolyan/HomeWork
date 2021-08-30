84b4d2e657a9e40e5f0b8792d69d791003bb1a94
Ilya <rd2d@list.ru>
2021-08-30 21:11:05 +0300

    complete lesson 4

---
+++ src/main/ObjectsAndClasses/lesson4/Color.java
@@ -0,0 +1,5 @@
+package lesson4;
+
+public enum Color {
+    BLACK,WHITE,SILVER
+}
---
+++ src/main/ObjectsAndClasses/lesson4/Loader.java
@@ -0,0 +1,26 @@
+package lesson4;
+
+public class Loader {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Cat cat1 = new Cat();
+
+        System.out.println(Cat.getCount());
+
+        for (int i=0;i<10;i++){
+            cat.feed(1000.0);
+        }
+        System.out.println(Cat.getCount());
+
+
+        cat.meow();
+        cat.pee();
+
+
+
+
+
+
+
+    }
+}
