4881e4195bba4e732ade950d8856e1a1753bb708
Ilya <rd2d@list.ru>
2021-08-31 23:12:45 +0300

    complete lesson 8

---
+++ src/main/ObjectsAndClasses/lesson8/Test.java
@@ -0,0 +1,17 @@
+package lesson8;
+
+import com.skillbox.airport.Aircraft;
+import com.skillbox.airport.Airport;
+import com.skillbox.airport.Terminal;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Airport.getInstance().getAllAircrafts());
+        System.out.println(Airport.getInstance().getAllAircrafts().size());
+
+
+
+
+
+    }
+}
