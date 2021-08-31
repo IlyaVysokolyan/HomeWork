f40888d7cbab1cdcba3babb362c6ab585bb0dcf6
Ilya <rd2d@list.ru>
2021-08-31 22:46:41 +0300

    complete lesson 5

---
+++ src/main/ObjectsAndClasses/lesson5/Loader.java
@@ -0,0 +1,17 @@
+package lesson5;
+
+public class Loader {
+    public static void main(String[] args) {
+        getKitten();
+        getKitten();
+        getKitten();
+
+
+
+
+    }
+    private static Cat getKitten(){
+        return new Cat(1100.0);
+    }
+
+}
