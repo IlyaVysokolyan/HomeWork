f188d25db3a12de24ec3d385213daaf3a1786425
Ilya <rd2d@list.ru>
2021-09-15 23:36:14 +0300

    complete ArraysAndCollections3

---
+++ src/main/ObjectsAndClasses/ArraysAndCollections3/Lesson1.java
@@ -0,0 +1,29 @@
+package ArraysAndCollections3;
+
+import java.util.List;
+import java.util.TreeSet;
+import java.util.regex.Pattern;
+
+public class Lesson1 {
+
+    public  static TreeSet <String> email = new TreeSet<>();
+    public static final String PATTERN_EMAIL = "^[A-Za-z0-9._%+-]+@[A-Z0-9a-z.-]+\\.[A-Za-z]{2,6}$";
+
+    public static void main(String[] args) {
+        if(args[0].equals("LIST")){
+         for(String e:email){
+             System.out.println(e);
+         }
+        }
+        else if(args[0].equals("ADD")){
+        if(args[1].matches(PATTERN_EMAIL)){
+            email.add(args[1]);
+            for(String e:email){
+                System.out.println(e+" ");
+            }
+        }else {
+            System.out.println("Неверный формат!!!");
+        }
+        }
+    }
+}
