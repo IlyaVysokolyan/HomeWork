3e98526fc818e9e4f1513f212d82cbc60de866ce
Ilya <rd2d@list.ru>
2021-10-11 21:47:00 +0300

    complete extendsAndPolymorphism Lesson1

---
+++ src/main/ObjectsAndClasses/extendsAndPolymorphism/DepositAccount.java
@@ -0,0 +1,20 @@
+package extendsAndPolymorphism;
+
+
+public class DepositAccount extends BankAccount {
+
+    public DepositAccount() {
+        amount = getMIN_BALANCE();
+    }
+
+    //Снять денег с депозитного счета
+    @Override
+    public void withdrawMoney(double sum) {
+        long time = System.currentTimeMillis();
+        if (((time - getDate().getTime()) / (24 * 60 * 60 * 1000)) < 31) {
+            System.out.println("Нельзя снять деньги депозитного счета раньше 31 дней после размещения");
+        } else {
+            super.withdrawMoney(sum);
+        }
+    }
+}
